e13e1a92329c02f33b776a10b2f4f27a895321fa
Tok-Amak <stev-pat@hotmail.fr>
2023-07-25 15:49:01 +0200

    first commit

---
+++ FrontEnd/script.js
@@ -0,0 +1,98 @@
+// url des different chemin de l'api
+
+const urlCategorie = "http://localhost:5678/api/categories"
+const urlWorks = "http://localhost:5678/api/works"
+
+// recuperer les donnée des Categorie dans l'api
+
+function getCategory() {
+    return fetch(urlCategorie).then((response) => response.json())
+    .then((categories) => {
+        return categories
+    })
+}
+
+// afficher les categorie sur le site ( format bouton radio )
+
+function displayCategories(categories) {
+    let display = ``
+    display += `<input type="radio" name="radio" class="filtre-radio" id="0" checked>`
+    display += `<label for="0">Tous</label>`
+        for (let compteur = 0; compteur < categories.length; compteur++){
+                display += `<input type="radio" name="radio" class="filtre-radio" id="${categories[compteur].id}">`
+                display += `<label for="${categories[compteur].id}">${categories[compteur].name}</label>`
+        }
+        document.querySelector(".filtre").innerHTML = display
+}
+
+// recuperer les donnée de Works dans l'api
+
+function getWorks() {
+    return fetch(urlWorks).then((response) => response.json())
+    .then((works) => {
+        return works
+    })
+}
+
+// afficher works sur le site
+
+function displayWorks(works, id) {
+    if (id === 0) {
+        let affichages = ``
+        for (let compteur = 0; compteur < works.length; compteur++){
+                affichages += `<figure>`
+                affichages += `<img src="${works[compteur].imageUrl}" alt="${works[compteur].title}">`
+                affichages += `<figcaption>${works[compteur].title}</figcaption>`
+                affichages += `</figure>`
+        }
+        document.querySelector(".gallery").innerHTML = affichages
+    } else {
+        console.log('filtre')
+    }
+    
+}
+
+// lire les different bouton filtre
+
+function getFilterId() {
+
+    let filtreSelector = document.querySelectorAll(".filtre input")
+
+    return filtreSelector.forEach(btn => {
+        btn.addEventListener("click", function(){
+            let id = btn.id
+            console.log(id)
+            
+            return id
+        })
+    })
+}
+
+//permet de filtrer les different categorie
+
+// function filter(works, id){
+    
+//     works.filter(dataFiltre => dataFiltre.categoryId == id)
+//         .forEach(categorieFiltre => {
+//             console.log(categorieFiltre)
+            
+//         })
+// }
+
+async function init() {
+    const works = await getWorks()
+    const id = getFilterId()
+    const categories = await getCategory()
+    displayCategories(categories)
+    displayWorks(works, id)
+    
+}
+
+
+init()
+
+
+
+
+// crée les bouton categorie automatiquement
+// filtrer les projet par rapport au bon bouton
