7fc369cdcc124f0d4c3bae71816add7b125772b6
Tok-Amak <stev-pat@hotmail.fr>
2023-08-02 08:46:02 +0200

    v2

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -153,8 +153,8 @@ const modalTriggers = document.querySelectorAll(".modal-trigger")
 // au click lance la function modalGetElement pour recuperer et afficher les element
 modalTriggers.forEach(trigger => trigger.addEventListener("click", function () {
     toggleModal()
-    modalGetElement()
 }))
+modalGetElement()
 
 // merme d'ouvrir la modale
 function toggleModal(){
@@ -165,18 +165,20 @@ function toggleModal(){
 async function modalGetElement() {
     const works = await getWorks()
     modalAffichage(works)
+    deleteButton()
 }
 
 // permet d'afficher les element 
 function modalAffichage(works) {
+    
     let affichages = ``
     for (let compteur = 0; compteur < works.length; compteur++){
             affichages += `<div class="element">`
-            affichages += `<img src="${works[compteur].imageUrl}" alt="${works[compteur].title}">`
-            affichages += `<svg xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11" fill="none">`
-            affichages += `<path d="M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" fill="white"/>`
-            affichages += `</svg>`
-            affichages += `<button>éditer</button>`
+            affichages += `     <img src="${works[compteur].imageUrl}" alt="${works[compteur].title}">`
+            affichages += `     <svg class="delete-button" xmlns="http://www.w3.org/2000/svg" width="9" height="11" viewBox="0 0 9 11" fill="none" id="${works[compteur].id}">`
+            affichages += `         <path d="M2.71607 0.35558C2.82455 0.136607 3.04754 0 3.29063 0H5.70938C5.95246 0 6.17545 0.136607 6.28393 0.35558L6.42857 0.642857H8.35714C8.71272 0.642857 9 0.930134 9 1.28571C9 1.64129 8.71272 1.92857 8.35714 1.92857H0.642857C0.287277 1.92857 0 1.64129 0 1.28571C0 0.930134 0.287277 0.642857 0.642857 0.642857H2.57143L2.71607 0.35558ZM0.642857 2.57143H8.35714V9C8.35714 9.70915 7.78058 10.2857 7.07143 10.2857H1.92857C1.21942 10.2857 0.642857 9.70915 0.642857 9V2.57143ZM2.57143 3.85714C2.39464 3.85714 2.25 4.00179 2.25 4.17857V8.67857C2.25 8.85536 2.39464 9 2.57143 9C2.74821 9 2.89286 8.85536 2.89286 8.67857V4.17857C2.89286 4.00179 2.74821 3.85714 2.57143 3.85714ZM4.5 3.85714C4.32321 3.85714 4.17857 4.00179 4.17857 4.17857V8.67857C4.17857 8.85536 4.32321 9 4.5 9C4.67679 9 4.82143 8.85536 4.82143 8.67857V4.17857C4.82143 4.00179 4.67679 3.85714 4.5 3.85714ZM6.42857 3.85714C6.25179 3.85714 6.10714 4.00179 6.10714 4.17857V8.67857C6.10714 8.85536 6.25179 9 6.42857 9C6.60536 9 6.75 8.85536 6.75 8.67857V4.17857C6.75 4.00179 6.60536 3.85714 6.42857 3.85714Z" fill="white"/>`
+            affichages += `     </svg>`
+            affichages += `     <button>éditer</button>`
             affichages += `</div>`
     }
     document.querySelector(".modal__galerie").innerHTML = affichages
@@ -201,6 +203,17 @@ function toggleAjoutModal(){
     modalAjoutContainer.classList.toggle("active")
 }
 
+// je recolte l'input ainsi que l'image que je vais previsualiser
+const inputImage = document.querySelector(".input-image")
+console.log(inputImage);
+const modaleImage = document.querySelector(".modale-image")
+console.log(modaleImage);
+
+modaleImage.addEventListener("change", function() {
+    modaleImage.src = URL.createObjectURL(inputImage.file[0])
+})
+
+
 // ======================================================================================  //
 //                                    liaison entre modal                                 //
 // ===================================================================================== //
@@ -216,3 +229,34 @@ modalAjoutReturn.addEventListener("click", function () {
     toggleAjoutModal()
     toggleModal()
 })
+
+
+// ======================================================================================  //
+//                                    suppression button                                  //
+// ===================================================================================== //
+
+function deleteButton() {
+    const deleteButton = document.querySelectorAll(".delete-button")
+    deleteButton.forEach(button => button.addEventListener("click", function(event) {
+        event.preventDefault()
+        fetch (`http://localhost:5678/api/works/${button.id}`, {
+            method: "DELETE",
+            headers: {
+                accept: "application/json",
+                "Content-type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        mode:"cors",
+        })
+        return false
+    }))
+}
+
+// permet de lire l'image
+new FileReader
+
+new Image()
+
+new FormData()
+
+new Blob()
